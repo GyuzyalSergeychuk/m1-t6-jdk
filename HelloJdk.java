0ee1e2604151d1fdb9a9f06578b28292f177a272
Gyuzyal <gyuzyal.sergeychuk@yandex.ru>
2022-12-04 16:22:20 +0300

    вывод на печать добавленна переменая

--- HelloJdk.java
+++ HelloJdk.java
@@ -1,7 +1,9 @@
 public class HelloJdk {
 
     public static void main(String[] args) {
+        var a = 5;
 
             System.out.println("Ура! Этот код работает!");
+        System.out.println(a);
     }
 }
